1a085d2889a46488aeb58adf5d74433cd6bc8af2
musamaGamma <johan0373@gmail.com>
2021-01-20 11:11:55 +0300

    deploy

---
+++ app/js/smoothScroll.js
@@ -0,0 +1,29 @@
+
+
+// function smoothScroll(element, duration) {
+//     let target = document.querySelector(element)
+//     let targetPosition = target.getBoundingClientRect().top 
+//     let startPosition = window.pageYOffset;
+//     let distance = targetPosition - startPosition;
+//     let startTime = null
+
+//     function animation(currentTime) {
+//         if(startTime === null) startTime = currentTime
+//         let timeElapsed = currentTime - startTime
+//         let run = ease(timeElapsed, startPosition, distance, duration)
+//         window.scrollTo(0, run)
+//         if(timeElapsed < duration) requestAnimationFrame(animation);
+//     }
+//     function ease(t, b, c, d) {
+//                   t /= d/2
+//                   if(t < 1) return c/2*t*t  + b;
+//                   t--;
+//                   return -c/2 * (t*(t-2)- 1) + b
+//     }
+
+//     requestAnimationFrame(animation)
+// }
+
+// document.querySelector(".works").addEventListener("click", ()=> {
+//     smoothScroll("#projects", 1000)
+// })
